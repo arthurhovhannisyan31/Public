c49fb6ca2fe0b85357f1c6c0cf7e961c5317945c
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2019-11-17 18:12:18 +0400

    T-2-1 Added router, containers and routes directories.

---
+++ src/containers/not-found/not-found.container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { NotFound as NotFoundComponent } from '../../components/error'
+
+const NorFound = () => {
+
+  return (
+    <div>
+      <NotFoundComponent/>
+    </div>
+  )
+}
+
+export default NorFound
