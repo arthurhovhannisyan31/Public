0d7af942e3250cfabb97e36eccfb3472019b778a
arthurhovhannisyan31 <43288945+arthurhovhannisyan31@users.noreply.github.com>
2019-11-18 11:25:16 +0400

    changed index file render function.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from "react"
-import ReactDOM from "react-dom"
+import { render } from "react-dom"
 import * as serviceWorker from "./serviceWorker"
 import Root from './containers/root'
 
-ReactDOM.render(<Root/>, document.getElementById("root"))
+const root = document.getElementById("root")
+
+render(<Root/>, root)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
