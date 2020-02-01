5d55c71ca9d771edf410f5f99baccc0c2bc6f514
arthurhovhannisyan31 <arthurhovhannisyan31@gmail.com>
2020-02-01 23:48:50 +0400

    T-5-1-2 Minor code cleanup.

--- src/containers/welcome/welcome.container.js
+++ src/containers/welcome/welcome.container.js
@@ -5,52 +5,99 @@ import React from 'react'
 // local components
 import ActionButton from "../../components/ui/button/action-button"
 import Icon from "../../components/ui/icons"
+import ContainedButton from "../../components/ui/button/contained-button"
 // local constants
 // local styles
 import './welcome.style.scss'
+
 
 const Welcome = () => {
 
   return (
     <div className='welcome'>
       <span>span</span>
-      <ActionButton>
-        <Icon
-          label='add'
-          fillMain='white'
-          fillOpacity={1}
-        />
-        <span>some text</span>
-      </ActionButton>
-      <ActionButton
-        floating
+      <ContainedButton
+
       >
-        <Icon
-          label='add'
-          fillMain='white'
-          fillOpacity={1}
-        />
-      </ActionButton>
-      <ActionButton
-        disabled
-      >
-        <Icon
-          label='add'
-          fillMain='white'
-          fillOpacity={1}
-        />
-        <span>some text</span>
-      </ActionButton>
-      <ActionButton
-        disabled
-        floating
-      >
-        <Icon
-          label='add'
-          fillMain='white'
-          fillOpacity={1}
-        />
-      </ActionButton>
+        <span>sample text</span>
+      </ContainedButton>
+      <div>
+        <span>Action buttons</span>
+        <div
+          style={{
+            display: 'flex',
+            'flex-flow': 'row'
+          }}
+        >
+          <ActionButton>
+            <Icon
+              label='add'
+              fillMain='white'
+            />
+            <span>&nbsp;&nbsp;some text</span>
+          </ActionButton>
+          <ActionButton
+            floating
+          >
+            <Icon
+              label='add'
+              fillMain='white'
+            />
+          </ActionButton>
+          <ActionButton
+            disabled
+          >
+            <Icon
+              label='add'
+              fillMain='white'
+            />
+            <span>&nbsp;&nbsp;some text</span>
+          </ActionButton>
+          <ActionButton
+            disabled
+            floating
+          >
+            <Icon
+              label='add'
+              fillMain='white'
+            />
+          </ActionButton>
+          <ActionButton
+            color='secondary'
+          >
+            <Icon
+              label='add'
+            />
+            <span>some text</span>
+          </ActionButton>
+          <ActionButton
+            floating
+            color='secondary'
+          >
+            <Icon
+              label='add'
+            />
+          </ActionButton>
+          <ActionButton
+            disabled
+            color='secondary'
+          >
+            <Icon
+              label='add'
+            />
+            <span>some text</span>
+          </ActionButton>
+          <ActionButton
+            disabled
+            floating
+            color='secondary'
+          >
+            <Icon
+              label='add'
+            />
+          </ActionButton>
+        </div>
+      </div>
     </div>
   )
 }
